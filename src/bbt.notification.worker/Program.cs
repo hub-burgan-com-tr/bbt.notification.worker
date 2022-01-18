cbd25d494a3e3d3eae8d2a5c2ae152c8f0072155
DoganTahir <dogan.tahir18@gmail.com>
2022-01-18 11:55:55 +0300

    add dockerfile and create project

---
+++ src/bbt.notification.worker/Program.cs
@@ -0,0 +1,10 @@
+using bbt.notification.worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
