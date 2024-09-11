3aaf15fa4b5bacc21d794e09c3b96bcacfd7935e
ucaremre <emreucar@outlook.com>
2024-09-11 11:21:26 +0300

    tckn ,tax no - get customerno

---
+++ src/bbt.notification.worker/Models/CustomerModel.cs
@@ -0,0 +1,106 @@
+﻿namespace bbt.notification.worker.Models
+{
+    public class CustomerBaseRequestModel
+    {
+        public CustomerBaseRequestModel()
+        {
+            Page = 1;
+
+            Size = 1;
+        }
+
+        public int Page { get; set; }
+
+        public int Size { get; set; }
+    }
+
+
+    public class CustomerSearchRequestModel : CustomerBaseRequestModel
+    {
+        public CustomerSearchRequestModel() : base()
+        {
+        }
+
+        public string? Name { get; set; }
+    }
+
+    public class CustomerRequestModel : CustomerBaseRequestModel
+    {
+        public CustomerRequestModel() : base()
+        {
+        }
+
+        public CustomerName? Name { get; set; }
+        public string? IdentityNumber { get; set; }
+        public long? CustomerNumber { get; set; }
+    }
+
+    public class CustomerResponseModel
+    {
+        public int RecordCount { get; set; }
+        public int ReturnCode { get; set; }
+        public string? ReturnDescription { get; set; }
+        public List<CustomerModel>? CustomerList { get; set; }
+    }
+
+    public class CustomerModel
+    {
+        public long CustomerNumber { get; set; }
+        public CustomerName? Name { get; set; }
+        public string? CitizenshipNumber { get; set; }
+        public string? TaxNo { get; set; }
+        public bool IsStaff { get; set; }
+        public GsmPhone? GsmPhone { get; set; }
+        public string? Email { get; set; }
+        public string? BusinessEmail { get; set; }
+        public string? BusinessLine { get; set; }
+        //public Device? Device { get; set; }
+        public long BranchCode { get; set; }
+        public string? BranchName { get; set; }
+        public string? PassportNo { get; set; }
+        public string? RecordStatus { get; set; }
+        public CustomerAddress? CustomerAddress { get; set; }
+
+        public string? IdentityNumber
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(CitizenshipNumber))
+                {
+                    return TaxNo;
+                }
+
+                return CitizenshipNumber;
+            }
+        }
+    }
+
+    public class GsmPhone
+    {
+        public string? Country { get; set; }
+        public string? Prefix { get; set; }
+        public string? Number { get; set; }
+    }
+
+    public class CustomerName
+    {
+        public string? First { get; set; }
+        public string? Last { get; set; }
+    }
+
+    public class Device
+    {
+        public string? Label { get; set; }
+        public string? DeviceId { get; set; }
+    }
+
+    public class CustomerAddress
+    {
+        public string District { get; set; } = default!;
+        public string Street { get; set; } = default!;
+        public string AddressDetail { get; set; } = default!;
+        public string Town { get; set; } = default!;
+        public string City { get; set; } = default!;
+        public string Address { get; set; } = default!;
+    }
+}
---
+++ src/bbt.notification.worker/Services/CustomerApiCall.cs
@@ -0,0 +1,63 @@
+﻿using bbt.notification.worker.Helper;
+using bbt.notification.worker.Models;
+using Elastic.Apm.Api;
+
+namespace bbt.notification.worker
+{
+    public class CustomerApiCall
+    {
+        private readonly ITracer _tracer;
+        private readonly ILogHelper _logHelper;
+        private readonly IConfiguration _configuration;
+        public CustomerApiCall(
+        ITracer tracer, ILogHelper logHelper, IConfiguration configuration
+      )
+        {
+            _tracer = tracer;
+            _logHelper = logHelper;
+            _configuration = configuration;
+        }
+        public async Task<long> GetCustomer(string customerSearchNumber)
+        {
+            long customerNo = 0;
+
+            await _tracer.CaptureTransaction("GetCustomer", ApiConstants.TypeRequest, async () =>
+            {
+                try
+                {
+                    var customerSearchRequestModel = new CustomerSearchRequestModel();
+                    customerSearchRequestModel.Name = customerSearchNumber;
+
+                    var path = _configuration.GetSection("CustomerApiUrl").Value;
+
+                    var response = await ApiHelper.ApiClient.PostAsJsonAsync(path, customerSearchRequestModel);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var customerResponseModel = await response.Content.ReadAsAsync<CustomerResponseModel>();
+
+                        if (customerResponseModel != null && customerResponseModel.CustomerList != null && customerResponseModel.CustomerList.Count > 0)
+                        {
+                            customerNo = customerResponseModel.CustomerList[0].CustomerNumber;
+                        }
+                        else
+                        {
+                            _logHelper.LogCreate(customerSearchNumber, false, "GetCustomer", customerResponseModel?.ReturnDescription ?? "GetCustomer Return Empty");
+                        }
+                    }
+                    else
+                    {
+                        _logHelper.LogCreate(customerSearchNumber, false, "GetCustomer", "CustomerApi Call Error:");
+                    }
+                }
+                catch (Exception e)
+                {
+                    _logHelper.LogCreate(customerSearchNumber, false, "GetCustomer", e.Message);
+                    _tracer.CaptureException(e);
+                }
+            });
+
+            return customerNo;
+        }
+    }
+}
